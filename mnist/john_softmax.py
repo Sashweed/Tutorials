6854663e78c9a931a1aaeeaa4442dc275f695e18
Sashweed <oscillate.darkly@gmail.com>
2016-12-18 16:46:17 +1100

    added default data dir
    F me … !

--- mnist/john_softmax.py
+++ mnist/john_softmax.py
@@ -152,11 +152,10 @@ correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print(sess.run(accuracy, feed_dict={x: mnist.test.images,
                                       y_: mnist.test.labels}))
-
+"""
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
                       help='Directory for storing input data')
   FLAGS, unparsed = parser.parse_known_args()
   tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
-"""
